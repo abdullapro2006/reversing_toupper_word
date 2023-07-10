4072e1b57e45b6320f58491542f2f9d436d5599d
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-10 15:11:46 +0400

    the third task completed

---
+++ Task reversing a word/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string word = "java";
+        string result = Convert.ToString(word);
+        for (int i = word.Length-1; i >= 0 ; i--)
+        {
+            word = word.ToUpper();
+            Console.Write(word[i]);
+
+        }
+        Console.Read();
+    }
+}
